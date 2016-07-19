408d2cd0d5cdaa1b82505c47d4bdc5fbe5c24dcd
bufitta <pikata88@mail.ru>
2016-07-19 20:45:42 +0300

    add urls form, admin_page, thanks_page

---
+++ order_project/urls.py
@@ -0,0 +1,27 @@
+"""order_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from my_app.views import order_form
+from my_app.views import order_table
+from my_app.views import thanks_for_order
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^form/', order_form, name='form'),
+    url(r'^admin_page/', order_table, name='admin_page'),
+    url(r'^thanks_page/', thanks_for_order, name='thanks_page'),
+]
